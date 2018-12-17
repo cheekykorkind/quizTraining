3e6c7164c3e4da5e14755066d2e2d89a277d9746
Yuhei Okazaki <okazaki@fusic.co.jp>
2018-12-17 20:02:32 +0900

    Answererを追加

--- src/views/answerer/Answer.vue
+++ src/views/answerer/Answer.vue
@@ -1,15 +1,22 @@
 <template>
-  <h1>Answer</h1>
+  <div>
+    <h1>Answer</h1>
+    <div v-if="currentUser">ログイン中のユーザ：{{ currentUser.name }}</div>
+    <div v-if="currentQuestion">現在の問題：{{ currentQuestion.sentence }}</div>
+  </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
+import Answerer from "./mixins/Answerer";
+
 export default {
   name: "Answer",
-  components: {},
-  data() {
-    return {};
-  },
-  created() {},
-  methods: {}
+  mixins: [Answerer],
+  computed: mapState({
+    currentUser: state => state.user.current,
+    questions: state => state.question.list,
+    currentQuestion: state => state.question.current
+  })
 };
 </script>
