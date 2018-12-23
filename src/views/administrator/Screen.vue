4012572cdb581ca0bf1dd98a0340fd01ee742846
jslee <lighthouse2.dev@gmail.com>
2018-12-23 17:45:07 +0900

    :+1: 画像を表示するように対応

--- src/views/administrator/Screen.vue
+++ src/views/administrator/Screen.vue
@@ -1,19 +1,35 @@
 <template>
   <div style="padding: 2vh 2vw;">
 
-    <div style="border: solid 1px; height: 45vh;">
-      <H2> お題 </H2>
-      <div style="width: 70%; float: left; text-align: left; padding: 0 1.5rem;">
-        {{ currentQuestion }}
+    <div style="">
+      <div style="text-align: left; padding: 0 1.5rem; font-size: 2rem;">
+        <label> お題 : </label> {{ currentQuestion }}
       </div>
-      <div style="width: 30%; float: left;">
-        イメージ
-        {{ configs[0]['.value'] }}
+      <div style="
+        height: 45vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      ">
+        <b-img
+          v-if="url"
+          :src="url"
+          fluid
+          alt="Fluid image"
+        />
       </div>
     </div>
 
-    <div style="border: solid 1px; height: 45vh;">
-      <H2> 回答者 </H2>
+    <div style="height: 30vh; padding-top: 1rem;">
+      <label style="
+        background-color: lightseagreen;
+        color: rgb(255, 255, 255);
+        padding: 0.3rem 1rem;
+        border-radius: 20px;
+        font-weight: bold;
+        font-size: 1.4rem;"
+      > 回答者 </label>
+      {{ configs[0]['.value'] }}
     </div>
 
   </div>
@@ -32,7 +48,8 @@ export default {
   components: {},
   data() {
     return {
-      currentQuestion: ''
+      currentQuestion: '',
+      url: null
     };
   },
   computed: {
@@ -43,20 +60,43 @@ export default {
   created() {
     // お題を取得
     this.getCurrentQuestionKey();
+
   },
   mounted() {},
   methods: {
+    /**
+     * 現在のお題のキーを取得
+     */
     getCurrentQuestionKey() {
       firebase.database().ref('questions/currentQuestion').once('value')
         .then( snapshot => {
+          // お題を取得
           this.getCurrentQuestion(snapshot.val());
         });
     },
+    /**
+     * お題を取得
+     */
     getCurrentQuestion(currentQuestionKey) {
-      firebase.database().ref(`questions/${currentQuestionKey}/sentence`).once('value')
+      firebase.database().ref(`questions/${currentQuestionKey}`).once('value')
         .then( snapshot => {
-          this.currentQuestion = snapshot.val();
+          this.currentQuestion = snapshot.val().sentence;
+
+          // お題のイメージを取得
+          if (snapshot.val().image) {
+            this.getCurrentQuestionImage(snapshot.val().image);
+          }
         });
+    },
+    /**
+     * お題のイメージを取得
+     */
+    getCurrentQuestionImage(imagePath) {
+      console.log('getImage');
+      const ref = firebase.storage().ref().child(imagePath);
+      const url = ref.getDownloadURL().then((url) => {
+        this.url = url
+      });
     }
   }
 };
@@ -65,5 +105,8 @@ export default {
 <style lang="scss" scoped>
 h2 {
   text-align: left;
+}
+.img-fluid {
+  height: 100%;
 }
 </style>
