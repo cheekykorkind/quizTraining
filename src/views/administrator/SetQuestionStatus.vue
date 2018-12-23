fdb49f41465f6357a9dc354dc4338ff19c8050ce
cheekykorkind <cheekorkind@gmail.com>
2018-12-23 18:27:30 +0900

    masterを取り込む

--- src/views/administrator/SetQuestionStatus.vue
+++ src/views/administrator/SetQuestionStatus.vue
@@ -1,13 +1,30 @@
 <template>
   <div>
-    <h1>SetQuestionStatus</h1>
-    {{ currentQuestion }}
+    <h1>お題進行</h1>
+
+    <!-- questions/{key}/visible --> <!-- true -->
+    <b-button variant="primary" @click="startQuiz()">
+      お題表示
+    </b-button>
     <br/>
-    {{ currentAnswerer }}
+
+    <!-- questions/{key}/isReady -->
+    <b-button variant="success" @click="startAnswer()">
+      回答受け
+    </b-button>
     <br/>
-    {{ currentQuestionKey }}
+
+    <!-- questions/{key}/answerer/{key}/answerble -->
+    <b-button variant="success" @click="startVote()">
+      投票受け
+    </b-button>
     <br/>
-    {{ currentAnswererKey }}
+
+    <!-- questions/{key}/visible --> <!-- false, 遷移(出題画面) -->
+    <b-button variant="danger" @click="endQuiz()">
+      お題終了
+    </b-button>
+    <br/>
   </div>
 </template>
 
@@ -34,6 +51,19 @@ export default {
     })
   },
   created() {},
-  methods: {}
+  methods: {
+    startQuiz: () => {
+      alert('startQuiz');
+    },
+    startAnswer: () => {
+      alert('startAnswer');
+    },
+    startVote: () => {
+      alert('startVote');
+    },
+    endQuiz: () => {
+      alert('endQuiz');
+    },
+  }
 };
 </script>
