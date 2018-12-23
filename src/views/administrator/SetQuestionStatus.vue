0850cb6242f872dae1b82142080a999d1d15b58d
Yuhei Okazaki <okazaki@fusic.co.jp>
2018-12-23 16:37:13 +0900

    画像表示のテスト

--- src/views/administrator/SetQuestionStatus.vue
+++ src/views/administrator/SetQuestionStatus.vue
@@ -1,18 +1,29 @@
 <template>
-  <h1>SetQuestionStatus</h1>
+  <div>
+    <h1>SetQuestionStatus</h1>
+    <b-img :src="url" fluid alt="Fluid image" />
+  </div>
 </template>
 
 <script>
 import Administrator from "./mixins/Administrator";
+import firebase from "firebase";
 
 export default {
   name: "SetQuestionStatus",
   mixins: [Administrator],
   components: {},
   data() {
-    return {};
+    return {
+      url: ''
+    };
   },
-  created() {},
+  created() {
+    const ref = firebase.storage().ref().child('questions/1.jpg');
+    const url = ref.getDownloadURL().then((url) => {
+      this.url = url
+    });
+  },
   methods: {}
 };
 </script>
