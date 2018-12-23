eadff310e459ec52b02d87417a9175d93cbac69b
cheekykorkind <cheekorkind@gmail.com>
2018-12-23 17:18:02 +0900

    push後currentAnswererを設定

--- src/views/answerer/Answer.vue
+++ src/views/answerer/Answer.vue
@@ -1,12 +1,7 @@
 <template>
   <div>
     <h1>Answer</h1>
-    <div v-if="currentUser">sss：{{ currentUser }}</div>
-    <div v-if="currentUser">sss：{{ currentUser.key }}</div>
     <div v-if="currentUser">ログイン中のユーザ：{{ currentUser.name }}</div>
-    <div v-if="currentUser">ユーザadministrator：{{ currentUser.administrator }}</div>
-    <div v-if="currentUser">ユーザanswerer：{{ currentUser.answerer }}</div>
-    <div v-if="currentUser">ユーザvoter：{{ currentUser.voter }}</div>
     <div v-if="currentQuestion">現在の問題：{{ currentQuestion.sentence }}</div>
     <div v-if="currentUser.answerer">あなたが回答者です</div>
     <b-button size="lg" variant="primary" @click="answer">回答する</b-button>
@@ -36,28 +31,54 @@ export default {
   methods: {
     answer: () => {
       firebase.auth().onAuthStateChanged(u => {
-        let uid = u.uid;
-
+        let _uid = u.uid;
+        let postData = {
+          uid : _uid,
+          voteNum : 0,
+        };
         if (u) {
-          firebase.database().ref('questions/3/answerer').orderByChild("uid").equalTo(uid).once('value').then(function(snapshot) {
-            let pushKey = Object.keys(snapshot.val()).pop();
-            // console.log(pushKey);
+          firebase.database().ref('questions/3').child('answerer')
+            .push(postData)
+            .then(reference => {
+              let pushKey = reference.path.pieces_.pop();
+              firebase.database().ref('questions/3').transaction(function (post) {
+                if (post.isReady) {
+                  post.currentAnswerer = pushKey;
+                  post.isReady = false;
+                }
 
-            return pushKey;
-          }).then(function(pushKey){
-            firebase.database().ref('questions/3').transaction(function (post) {
-              if (post.isReady) {
-                post.whoIsFirstAnswerer = pushKey;
-                post.isReady = false;
-              }
-
-              return post;
-            })
-          })
+                return post;
+              })
+            });
         }
-
       });
     },
+
+    // // 開発用。参考
+    // answerOld: () => {
+    //   firebase.auth().onAuthStateChanged(u => {
+    //     let uid = u.uid;
+
+    //     if (u) {
+    //       firebase.database().ref('questions/3/answerer').orderByChild("uid").equalTo(uid).once('value').then(function(snapshot) {
+    //         let pushKey = Object.keys(snapshot.val()).pop();
+    //         // console.log(pushKey);
+
+    //         return pushKey;
+    //       }).then(function(pushKey){
+    //         firebase.database().ref('questions/3').transaction(function (post) {
+    //           if (post.isReady) {
+    //             post.whoIsFirstAnswerer = pushKey;
+    //             post.isReady = false;
+    //           }
+
+    //           return post;
+    //         })
+    //       })
+    //     }
+
+    //   });
+    // },
 
     // // 開発用。参考
     // appendUser: () => {
